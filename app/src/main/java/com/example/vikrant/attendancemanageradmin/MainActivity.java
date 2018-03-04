b04bc610261baa1b17c75e65a338bfdabc320953
Vikrant Kataria <vik7am@gmail.com>
2018-03-04 20:28:46 +0530

    Added Firebase to Project

--- app/src/main/java/com/example/vikrant/attendancemanageradmin/MainActivity.java
+++ app/src/main/java/com/example/vikrant/attendancemanageradmin/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.vikrant.attendancemanageradmin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Toast.makeText(getApplicationContext(),"Welcome",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
